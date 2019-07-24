1cfb1d4cfaa3bc5de39243b4eea03abba1c04f37
Mauricio Collazos <ma0@contraslash.com>
2019-07-24 16:27:17 -0500

    first app component

---
+++ lecture_46/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.error(error)
+        )
+        return (
+            <div>
+                Latitude: 
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector("#root")
+)
